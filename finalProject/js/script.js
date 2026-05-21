0daa9cd7f918fc5be8294599dbc86b26f524a745
callersanti <callersanti@gmail.com>
2026-05-21 18:48:57 -0500

    Se corrigen títulos, se cambia icono busqueda por carro y se ajustan etiquetas

--- finalProject/js/script.js
+++ finalProject/js/script.js
@@ -1,6 +1,11 @@
-
+const menuButton = document.getElementById("menuButton");
+menuButton.addEventListener("click", function() {
+        console.log("Button clicked!")
+    })
     
 
-
+const linkMenu = () => {
+    
+}
 
 
