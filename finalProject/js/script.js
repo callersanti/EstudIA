b6eaf80c522887cb44cbc4cd44e33d76a4909212
callersanti <callersanti@gmail.com>
2026-05-21 19:05:20 -0500

    Se completan lar cards, se agregan links, data actualizada, estilos para secciones, y js para scroll de la pagina

--- finalProject/js/script.js
+++ finalProject/js/script.js
@@ -1,11 +1,13 @@
-const menuButton = document.getElementById("menuButton");
-menuButton.addEventListener("click", function() {
-        console.log("Button clicked!")
-    })
-    
+const MENU = "https://drive.google.com/file/d/1cp0smZaA-HCXjh9qsDNKvM5GE0l_8cdc/view?usp=sharing";
 
-const linkMenu = () => {
-    
-}
+const menuButton = document.getElementById("menuButton");
+menuButton.addEventListener("click", function () {
+    window.open(MENU, "_blank");
+});
+
+const orderButton = document.getElementById("orderButton");
+orderButton.addEventListener("click", function () {
+    document.getElementById("productos");
+});
 
 
