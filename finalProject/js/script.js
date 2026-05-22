4511497783845ea2b463057b5aeaf6857f709f4f
callersanti <callersanti@gmail.com>
2026-05-21 20:37:24 -0500

    Se agrega contador simple de carrito, se agrega un badge y se hacen pruebas responsive.

--- finalProject/js/script.js
+++ finalProject/js/script.js
@@ -10,4 +10,34 @@ orderButton.addEventListener("click", function () {
     document.getElementById("productos");
 });
 
+const badge = document.getElementById("badge");
+let cartCount = 0;
+
+const addButtons = document.querySelectorAll("#productos .btn");
+
+addButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    const fullCart = button.textContent === 'Borrar del carrito';
+
+    if (!fullCart) {
+      cartCount++;
+      button.textContent = 'Borrar del carrito';
+      button.classList.replace('btn-outline-light', 'btn-danger');
+    } else {
+      cartCount--;
+      button.textContent = 'Añadir al carrito';
+      button.classList.replace('btn-danger', 'btn-outline-light');
+    }
+
+    badge.textContent = cartCount;
+
+    if (cartCount > 0) {
+        badge.style.display = 'inline';
+      } else {
+        badge.style.display = 'none';
+      }
+      
+  });
+});
+
 
